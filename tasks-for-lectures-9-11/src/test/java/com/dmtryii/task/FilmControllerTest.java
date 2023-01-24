952b969ec2e5fade23f9ee9a049619c7bead37fa
dmtryii <dmitriy04tr@gmail.com>
2023-01-24 16:19:04 +0200

    added basic tests for FilmController

--- tasks-for-lectures-9-11/src/test/java/com/dmtryii/task/FilmControllerTest.java
+++ tasks-for-lectures-9-11/src/test/java/com/dmtryii/task/FilmControllerTest.java
@@ -2,6 +2,7 @@ package com.dmtryii.task;
 
 import com.dmtryii.task.dto.RestResponse;
 import com.dmtryii.task.dto.film.FilmSaveDto;
+import com.dmtryii.task.dto.film.FilmSearchDto;
 import com.dmtryii.task.model.Director;
 import com.dmtryii.task.model.Film;
 import com.dmtryii.task.repository.DirectorRepository;
@@ -18,15 +19,19 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
@@ -74,11 +79,94 @@ public class FilmControllerTest {
         assertThat(film.getDirector().getId()).isEqualTo(saveDto.getDirectorId());
     }
 
+    @Test
+    public void getFilmSuccess() throws Exception {
+        Director director = createDirector("New FirstName", "New LastName");
+        Film film = createFilm("New film", 0.9, director);
+
+        ResultActions response = mvc.perform(
+                get("/api/films/{id}",
+                film.getId()
+        ));
+
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(film.getId()))
+                .andExpect(jsonPath("$.title").value(film.getTitle()))
+                .andExpect(jsonPath("$.rating").value(film.getRating()))
+                .andExpect(jsonPath("$.releaseDate").value(film.getReleaseDate().toString()))
+                .andExpect(jsonPath("$.director.id").value(film.getDirector().getId()))
+                .andExpect(jsonPath("$.director.firstName").value(film.getDirector().getFirstName()))
+                .andExpect(jsonPath("$.director.lastName").value(film.getDirector().getLastName()));
+    }
+
+    @Test
+    public void getAllFilmsSuccess() throws Exception {
+        Director firstDirector = createDirector("FirstName", "FirstSurname");
+        Director secondDirector = createDirector("SecondName", "SecondSurname");
+        Director thirdDirector = createDirector("ThirdName", "ThirdSurname");
+
+        createFilm("title", 0.9, firstDirector);
+        createFilm("SecondTitle", 0.5, secondDirector);
+        createFilm("ThirdTitle", 0.2, thirdDirector);
+        createFilm("FourthTitle", 0.2, secondDirector);
+
+        ResultActions response = mvc.perform(
+                get("/api/films/")
+        );
+
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[*].director.id").exists())
+                .andExpect(jsonPath("$[*].director.firstName").exists())
+                .andExpect(jsonPath("$[*].director.lastName").exists());
+    }
+
+    @Test
+    public void updateFilmSuccess() throws Exception{
+        Director director = createDirector("New FirstName", "New LastName");
+        Film film = createFilm("New film", 0.9, director);
+        FilmSaveDto saveDto = getFilmSaveDto("Update title", 0.95, LocalDate.now(), director.getId());
+
+        mvc.perform(
+                put("/api/films/{id}", film.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(saveDto))
+                ).andExpect(status().isOk());
+
+        Film updateFilm = filmRepository.findById(film.getId()).orElse(null);
+        assertThat(updateFilm).isNotNull();
+        assertThat(updateFilm.getTitle()).isEqualTo(saveDto.getTitle());
+        assertThat(updateFilm.getRating()).isEqualTo(saveDto.getRating());
+        assertThat(updateFilm.getReleaseDate()).isEqualTo(saveDto.getReleaseDate());
+        assertThat(updateFilm.getDirector().getId()).isEqualTo(saveDto.getDirectorId());
+    }
+
+    @Test
+    public void deleteBookSuccess() throws Exception {
+        Director director = createDirector("New FirstName", "New LastName");
+        Film film = createFilm("New film", 0.9, director);
+
+        mvc.perform(
+              delete("/api/films/{id}", film.getId())
+        ).andExpect(status().isNoContent());
+
+        assertThat(filmRepository.findById(film.getId())).isEmpty();
+    }
+
     private Director createDirector(String firstName, String lastName) {
         Director director = new Director();
         director.setFirstName(firstName);
         director.setLastName(lastName);
         return directorRepository.save(director);
+    }
+
+    private Film createFilm(String title, Double rating, Director director) {
+        Film film = new Film();
+        film.setTitle(title);
+        film.setRating(rating);
+        film.setReleaseDate(LocalDate.now());
+        film.setDirector(director);
+        return filmRepository.save(film);
     }
 
     private FilmSaveDto getFilmSaveDto(String title, Double rating, LocalDate releaseDate, Long directorId) {
